e484e414ac036bceee3c17106ee6186dc6d6171d
ikerm <ikerm@LAPTOP-155AQCB8>
2023-01-09 21:56:12 -0600

    Agregar archivos de proyecto.

---
+++ expresiones 1/expresiones 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float a, b, resultado = 0;
+	cout << "dime el valor de a: "; cin >> a;
+	cout << "dime el valor de b: "; cin >> b;
+
+	resultado = (a / b) + 1;
+
+	cout.precision(2);
+	cout << "\nTu resultado es: " << resultado << endl;
+
+	return 0;
+
+}
